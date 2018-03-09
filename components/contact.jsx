f5c982317cb90e2bf4aec858dde25f6d4264c7e9
Radoslaw Podgorski <radoslaw.podgorski@gmail.com>
2018-03-09 12:50:27 +0000

    first commit

---
+++ components/contact.jsx
@@ -0,0 +1,37 @@
+var contacts = [
+  {
+    id: 1,
+    firstName: 'Jan',
+    lastName: 'Nowak',
+    email: 'jan.nowak@example.com',
+  },
+  {
+    id: 2,
+    firstName: 'Adam',
+    lastName: 'Kowalski',
+    email: 'adam.kowalski@example.com',
+  },
+  {
+    id: 3,
+    firstName: 'Zbigniew',
+    lastName: 'Koziol',
+    email: 'zbigniew.koziol@example.com',
+  }
+];
+
+var Contact = React.createClass({
+  propTypes: {
+    item: React.PropTypes.object.isRequired,
+  },
+
+  render: function() {
+    return (
+      <div className={'contactItem'}>
+        <img className={'contactImage'} src={'http://icons.veryicon.com/ico/System/100%20Flat%20Vol.%202/contacts.ico'}
+        <p className={'contactLabel'} Imię: {this.props.item.firstName} </p>
+        <p className={'contactLabel'} Nazwisko: {this.props.item.lastName} </p>
+        <a href={'mailto:' + this.props.item.email} className={contactEmail} value={this.props.item.email}> </a>
+    )
+  },
+});
+
---
+++ components/contactForm.jsx
@@ -0,0 +1,14 @@
+var ContactForm = React.createClass({
+  propTypes: {
+    contact: React.PropTypes.object.isRequired
+  },
+  render: function() {
+    return (
+      <form className={'contactForm'}>;
+      <input type={'text'} placeholder={'Imię'} value={this.props.contact.firstName}>;
+      <input type={'text'} placeholder={'Nazwisko'} value={this.props.contact.lastName}>;
+      <input type={'text'} placeholder={'email'} value={this.props.contact.email}>;
+      <button type={'submit'}>Dodaj kontakt</button>;
+      )
+  }
+})
