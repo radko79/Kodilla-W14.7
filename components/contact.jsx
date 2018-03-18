b07a67b673cb5771c666f0787d7b194adc0a88b0
Radoslaw Podgorski <radoslaw.podgorski@gmail.com>
2018-03-18 18:01:36 +0000

    fix problems, final commit

--- components/contact.jsx
+++ components/contact.jsx
@@ -19,6 +19,12 @@ var contacts = [
   }
 ];
 
+var contactForm = {
+  firstName: '',
+  lastName: '',
+  email: ''
+};
+
 var Contact = React.createClass({
   propTypes: {
     item: React.PropTypes.object.isRequired,
@@ -27,10 +33,11 @@ var Contact = React.createClass({
   render: function() {
     return (
       <div className={'contactItem'}>
-        <img className={'contactImage'} src={'http://icons.veryicon.com/ico/System/100%20Flat%20Vol.%202/contacts.ico'}
-        <p className={'contactLabel'} Imię: {this.props.item.firstName} </p>
-        <p className={'contactLabel'} Nazwisko: {this.props.item.lastName} </p>
-        <a href={'mailto:' + this.props.item.email} className={contactEmail} value={this.props.item.email}> </a>
+        <img className={'contactImage'} src="http://icons.veryicon.com/ico/System/100%20Flat%20Vol.%202/contacts.ico" />
+        <p className={'contactLabel'}> Imię: {this.props.item.firstName} </p>
+        <p className={'contactLabel'}> Nazwisko: {this.props.item.lastName} </p>
+        <a href={'mailto:' + this.props.item.email} className={'contactEmail'} value={this.props.item.email}> sad as... </a>
+      </div>
     )
   },
 });
--- components/contactForm.jsx
+++ components/contactForm.jsx
@@ -4,11 +4,12 @@ var ContactForm = React.createClass({
   },
   render: function() {
     return (
-      <form className={'contactForm'}>;
-      <input type={'text'} placeholder={'Imię'} value={this.props.contact.firstName}>;
-      <input type={'text'} placeholder={'Nazwisko'} value={this.props.contact.lastName}>;
-      <input type={'text'} placeholder={'email'} value={this.props.contact.email}>;
-      <button type={'submit'}>Dodaj kontakt</button>;
-      )
+      <form className={'contactForm'}>
+        <input type={'text'} placeholder={'Imię'} value={this.props.contact.firstName}/>
+        <input type={'text'} placeholder={'Nazwisko'} value={this.props.contact.lastName}/>
+        <input type={'text'} placeholder={'email'} value={this.props.contact.email}/>
+        <button type={'submit'}>Dodaj kontakt</button>
+      </form>
+      );
   }
 })
